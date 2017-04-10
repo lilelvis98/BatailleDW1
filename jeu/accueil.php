f3a55837e1bff7a4e4233e26c9863e36c379275f
VILLERMET QUENTIN p1507338 <p1507338@dhcpprov229-74.univ-lyon1.fr>
2017-04-10 17:16:16 +0200

    stable mais pas de co

--- jeu/accueil.php
+++ jeu/accueil.php
@@ -1,28 +1,3 @@
-<!DOCTYPE html>
-<html>
-	<head>
+<?php
 
-		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-		<link rel="stylesheet" type="text/css" href="css/style_normal.css">
-		<title>La Bataille Navale</title>
-		<?php
-			require_once('./fonctions.php');
-		?>
-
-	</head>
-	<body>
-
-		<h1>Bonjour et bienvenue, matelot !</h1>
-
-		<div id="boiteconnexion">
-			<form method='POST' action='./accueil.php'> <!-- Appeler une fonction php qui teste si les champs sont corrects -->
-				<label for="nomB">connexion</label>
-				<input id="pseudoJ" name="pseudoJoueur" type="text" placeholder="Pseudo" />
-				<input id="mdp" name="motDePasse" type="password" placeholder="Mot de Passe" />
-				<input id="clico" name="cliqueconnexion" type="submit" value="Se Connecter" />
-			</form>
-		</div>
-		<!-- Div d'erreur de co -->
-		
-	</body>
-</html>
+?>
