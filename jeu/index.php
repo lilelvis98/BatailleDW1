9b338059be1f80eaf4ace142c420524327210db6
VILLERMET QUENTIN p1507338 <p1507338@b710l0805.univ-lyon1.fr>
2017-04-14 11:18:01 +0200

    debuts connexion

--- jeu/index.php
+++ jeu/index.php
@@ -1,29 +1,27 @@
 <!DOCTYPE html>
 <html>
 	<head>
-
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 		<link rel="stylesheet" type="text/css" href="css/style_normal.css">
-		<title>La Bataille Navale</title>
-		<?php
-			require_once('./fonctions.php');
-		?>
 
+		<title>La Bataille Navale</title>
 	</head>
+
 	<body>
 		<?php
+			require_once('./fonctions.php');
+
 			//connexion à la base de données
 			connexion_bd();
 		?>
-		<br/>
-		<h1>Bonjour et bienvenue, matelot !</h1>
-		<br/><br/>
+
+		<h1>Bonjour et bienvenue, matelot !</h1><br />
 
 		<!-- Formulaire de connexion -->
 		<div id="boiteconnexion">
 			<div id="centrer">
 				<p>Connexion</p>
-				<form method='POST' action='./accueil.php'>
+				<form method='POST' action='./connexion.php'>
 					<p>
 						<input id="pseudoJ" name="pseudoJoueur" type="text" placeholder="Pseudo" />
 					</p>
@@ -37,11 +35,13 @@
 			</div>
 		</div>
 
+<!-- Div d'erreur de co -->
+
 		<!-- Formulaire d'inscription -->
 		<div id="boiteinscription">
 			<div id="centrer">
 				<p>
-					<form method='POST' action='./accueil.php'>
+					<form method='POST' action='./inscription.php'>
 						<p>
 							<input id="clici" name="cliqueinscription" type="submit" value="S'inscrire" class="inscription"/>
 						</p>
@@ -49,10 +49,6 @@
 				</p>
 			</div>
 		</div>
-
-
-		<!-- Div d'erreur de co -->
-		
 	</body>
 </html>
 
